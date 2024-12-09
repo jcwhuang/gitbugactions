50bdc07fa78ce4760ec48d325e4128617dc793b1
Jade Huang <jadhuang@microsoft.com>
2024-12-09 11:40:33 -0800

    pass full docker

--- gitbugactions/actions/actions.py
+++ gitbugactions/actions/actions.py
@@ -258,7 +258,7 @@ class Act:
         self.flags += f" --memory={Act.__MEMORY_LIMIT}"
         self.flags += "'"
 
-        self.__DEFAULT_RUNNERS = f"-P ubuntu-latest={base_image}"
+        self.__DEFAULT_RUNNERS = f"-P ubuntu-latest=ghcr.io/catthehacker/{base_image}"
         self.timeout = timeout
         self.fail_strategy = fail_strategy
 
