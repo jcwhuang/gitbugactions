ba23f376f8d54252371d2a1eda99b9f31541e8e1
Jade Huang <jadhuang@microsoft.com>
2025-01-10 14:29:31 -0800

    look in GOPATH

--- gitbugactions/actions/go/go_workflow.py
+++ gitbugactions/actions/go/go_workflow.py
@@ -47,7 +47,7 @@ class GoWorkflow(GitHubWorkflow):
                         i,
                         {
                             "name": "gitbug-actions install go-junit-report",
-                            "run": "go install github.com/jstemmer/go-junit-report/v2@v2.0.0 && ls -la $HOME/go/bin && echo $HOME/go/bin >> $GITHUB_PATH",
+                            "run": "go install github.com/jstemmer/go-junit-report/v2@v2.0.0 && ls -la $GOPATH/bin && ls -la $HOME/go/bin && echo $HOME/go/bin >> $GITHUB_PATH",
                         },
                     )
                     # We need to generate the vendor to keep dependencies
